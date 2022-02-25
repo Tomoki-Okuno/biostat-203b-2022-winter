29a16cf9b0eff5301dced61c0110cc9e136895ad
Tomoki Okuno <tomokiokuno0528@g.ucla.edu>
2022-02-25 20:58:27 +0000

    This is the final version for HW3 submission.

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -2,8 +2,8 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(datasets)
-library(tidyverse)
 library(lubridate)
+library(tidyverse)
 
 # import icu_cohort data as a tibble
 data <- read_rds("icu_cohort.rds")
